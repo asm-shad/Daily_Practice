445b62c10294834cf375650fe3dcdc2792145c7f
ASM Shad <asmshad@gmail.com>
2026-05-11 12:41:01 +0600

    Day29 Schedule task and run every hour or day

---
+++ day_29.py
@@ -0,0 +1,75 @@
+"""
+depends on:
+ - Day 7 of web scraping
+
+Fetch crypto data every hour automatically
+
+"""
+
+import os
+import csv
+import requests
+from datetime import datetime
+import schedule
+import time
+
+API_URL = "https://api.coingecko.com/api/v3/coins/markets"
+PARAMS = {
+    'vs_currency': 'usd',
+    'order': 'market_cap_desc',
+    'per_page': 10,
+    'page': 1,
+    'sparkline': False
+}
+
+CSV_FILE = 'crypto_prices.csv'
+
+def fetch_crypto_data():
+    response = requests.get(API_URL, params=PARAMS)
+    return response.json()
+
+def save_to_csv(data):
+    file_exists = os.path.isfile(CSV_FILE)
+
+    with open(CSV_FILE, 'a', newline='') as file:
+        writer = csv.writer(file)
+
+        if not file_exists:
+            writer.writerow(["timestamp", "coin", "price"])
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+
+        for coin in data:
+            writer.writerow([timestamp, coin["id"], coin['current_price']])
+    print("✅ Data saved to csv.")
+    times = []
+    prices = []
+
+    with open(CSV_FILE, newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row["coin"] == coin_id:
+                times.append(row['timestamp'])
+                prices.append(float(row['price']))
+
+    if not times:
+        print(f"No daa found for {coin_id}")
+        return
+    
+    plt.figure(figsize=(10,5))
+    plt.plot(times, prices, marker='o')
+    plt.tight_layout()
+    plt.grid()
+    plt.show()
+
+def job():
+    print("Fetching data hourly...")
+    crypto_data = fetch_crypto_data()
+    save_to_csv(crypto_data)
+
+schedule.every().hour.at(":00").do(job)
+# schedule.every().day.at("10:30").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
