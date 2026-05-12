041af2fb3ff4ce3e7cc87104a695b49a16f62fd7
ASM Shad <asmshad@gmail.com>
2026-05-12 14:13:02 +0600

    Day32 File organiser

---
+++ day_32.py
@@ -0,0 +1,56 @@
+"""
+ Challenge: File Sorter by Type
+
+Goal:
+- Scan the current folder (or a user-provided folder)
+- Move files into subfolders based on their type:
+    - .pdf → PDFs/
+    - .jpg, .jpeg, .png → Images/
+    - .txt → TextFiles/
+    - Others → Others/
+- Create folders if they don't exist
+- Ignore folders during the move
+
+Teaches: File system operations, automation, file handling with `os` and `shutil`
+"""
+
+import os
+import shutil   # Can move, copy files
+
+EXTENSION_MAP = {
+    "PDFs": [".pdf"],
+    "Images": [".jpg", ".jpeg", ".png", ".webp", ".svg"],
+    "TextFiles": [".txt", ".docx", ".doc", ".json"],
+    "Software": [".exe", ".iso"],
+    "DB": [".sql"]
+}
+
+def get_destination_folder(filename):
+    ext = os.path.splitext(filename)[1].lower()
+    for folder, extensions in EXTENSION_MAP.items():
+        if ext in extensions:
+            return folder
+    return "Others"
+
+def sort_files(folder_path):
+    for file in os.listdir(folder_path):
+        full_path = os.path.join(folder_path, file)
+
+        if os.path.isfile(full_path):
+            dest_folder = get_destination_folder(file)
+            dest_path = os.path.join(folder_path, dest_folder)
+            os.makedirs(dest_path, exist_ok=True)
+
+            # move the file
+            shutil.move(full_path, os.path.join(dest_path, file))
+            print(f"Moved: {file} -> {dest_folder}")
+
+if __name__ == "__main__":
+    folder = input("Enter the folder path or leave blank: ").strip()
+    folder = folder or os.getcwd()
+
+    if not os.path.isdir(folder):
+        print("Invalid Directory.")
+    else:
+        sort_files(folder)
+        print("✅ Sorting Completes")
